2b92fe04e185eee1e0f8eb872df306558fbd13e6
PJzuza <PJzuza@gmail.com>
2018-01-11 01:19:43 +0700

    Temp fixed Counterstrike skill issue
    - Added a check for counterstrike skill though it's still buggy as hell :P

--- MeleeCuffbyCop/MeleeChance.lua
+++ MeleeCuffbyCop/MeleeChance.lua
@@ -1,14 +1,18 @@
 if RequiredScript == "lib/units/beings/player/playerdamage" then
 
 	-- create an overided function to a player so it won't fuck** up the whole function 
-	local overideplayerdamagemelee = PlayerDamage.damage_melee
+	local overide_player_damage_melee = PlayerDamage.damage_melee
 	local getting_cuffed_melee_chance = 20 -- Change this number from 0(cops won't cuff a player) up to 100(cops always cuff a player)
 	function PlayerDamage:damage_melee(attack_data)
-		overideplayerdamagemelee(self, attack_data)
+		overide_player_damage_melee(self, attack_data)
 		if not self:_chk_can_take_dmg() then
 			return
 		end
-			
+		
+			-- preparing somee stuffs
+			local getting_cuffed_chance = math.random(0,100)
+			local player_can_counter_strike_cops = managers.player:has_category_upgrade("player", "counter_strike_melee")
+			local player_can_counter_strike_clk =  managers.player:has_category_upgrade("player", "counter_strike_spooc")
 			-- check if the player got tased | if so check if the player is on swan song or not.
 			if self._unit:movement():tased() then
 				if not self._unit:character_damage().swansong then
@@ -16,31 +20,40 @@ if RequiredScript == "lib/units/beings/player/playerdamage" then
 					if alive(self._unit) then
 						-- check if the player got meleed then a cop can cuff the player instantly.
 						if self:_chk_can_take_dmg() then
-						managers.player:set_player_state("arrested")
-						return self._current_state
+							managers.player:set_player_state("arrested")
+							return self._current_state
 						end
 					end
 				end
 			end
-
-		local getting_cuffed_chance = math.random(0,100)
+			
 			-- run a random number | if true then the player will get cuffed by cops because of getting meleed.
 			if getting_cuffed_chance < getting_cuffed_melee_chance then
 				-- if the player is on swansong, beed out or incapacitated state then the player won't get cuffed.
 				if self._unit:character_damage().swansong or self._bleed_out or self:incapacitated() then
 				
+				-- check if the player has counterstrike skill basic or ace then cops can't cuff you (for now...)
+				-- Serious bug. You gain auto counter somehow and sometimes :\ (but luckily the cloaker counter is normal so be on guard when facing him.)
+				elseif player_can_counter_strike_cops and self._unit:movement():current_state().in_melee then
+					if player_can_counter_strike_clk and self._unit:movement():current_state().in_melee then
+							self._unit:movement():current_state():discharge_melee()
+							return "countered"
+					else
+						self._unit:movement():current_state():discharge_melee()
+							return "countered"
+					end 
+				-- if others states from above 	
 				else
 					managers.player:set_player_state("arrested")
 					return self._current_state
 				end
 			end
-
 	end
 end
 
 --[[
 Thing I still have to do
-	* Fixed some serious bugs. (no timer if you got cuffed while you got tased at the same time, every enemies can cuff a player for now)
+	* Fixed some serious bugs. (auto counter, no timer if you got cuffed while you got tased at the same time, every enemies can cuff a player for now, auto counter is cheating to me... though I am trying to fix it)
 	* Exclude some types of enemies and special enemies like Dozer, Taser, Sniper, Shield may be...
 	* Make it works both client side and a host side. (now it works only if you have this mod)
 	* I may add an option to enable-disable this mod.
