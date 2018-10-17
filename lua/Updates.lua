e25cf4dd3a8d12455a393b9973ea8ecd5e702b58
PJzuza <PJzuza@gmail.com>
2018-10-18 02:55:47 +0700

    Prepared auto-updater once again
    - Updated changelog.txt
    - Fixed updates.lua

--- lua/Updates.lua
+++ lua/Updates.lua
@@ -2,7 +2,9 @@ if UpdateThisMod then
 	UpdateThisMod:Add({
 		mod_id = 'MeleeCuffbyCop',
 		data = {
-			modworkshop_id = 21693
+			modworkshop_id = 21693,
+			dl_url = "https://github.com/PJzuza/MeleeCuffbyCop/raw/master/updates/MCbC.zip",
+			info_url = "https://raw.githubusercontent.com/PJzuza/MeleeCuffbyCop/master/mod.txt"
 		}
 	})
 end
