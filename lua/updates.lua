d9219ad2e9d8f63db7efa7875e4d732d57ee06b3
PJzuza <PJzuza@gmail.com>
2018-10-18 03:00:10 +0700

    Readded updates.lua
    To avoid case sensitive :P

---
+++ lua/updates.lua
@@ -0,0 +1,10 @@
+if UpdateThisMod then
+	UpdateThisMod:Add({
+		mod_id = 'MeleeCuffbyCop',
+		data = {
+			modworkshop_id = 21693,
+			dl_url = 'https://github.com/PJzuza/MeleeCuffbyCop/raw/master/updates/MCbC.zip',
+			info_url = 'https://raw.githubusercontent.com/PJzuza/MeleeCuffbyCop/master/mod.txt'
+		}
+	})
+end
