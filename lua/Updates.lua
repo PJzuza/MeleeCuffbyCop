afa6234cccddb0d3b619d155d1372c6f0ea7a89c
shinrax2 <shinra.ubu1@yahoo.de>
2018-10-17 16:02:34 +0200

    fix auto-update

--- lua/Updates.lua
+++ lua/Updates.lua
@@ -1,19 +1,8 @@
-local superblt = false
-for _, mod in pairs(BLT and BLT.Mods:Mods() or {}) do
-	if mod:GetName() == "SuperBLT" and mod:IsEnabled() then
-		superblt = true
-		break
-	end			
-end
-if superblt == false then
-	if UpdateThisMod then
-		UpdateThisMod:Add({
-			mod_id = 'MeleeCuffbyCop',
-			data = {
-				dl_url = 'https://raw.githubusercontent.com/PJzuza/MeleeCuffbyCop/blob/master/updates/MCbC.zip',
-				info_url = 'https://raw.githubusercontent.com/PJzuza/MeleeCuffbyCop/master/mod.txt'
-			}
-		})
-	end
-
+if UpdateThisMod then
+	UpdateThisMod:Add({
+		mod_id = 'MeleeCuffbyCop',
+		data = {
+			modworkshop_id = 21693
+		}
+	})
 end
